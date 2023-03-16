9b2d3ee0ed058e8c56e23face84c53ec86b04d76
Delanna Do <50529918+Ddelanna@users.noreply.github.com>
2023-03-16 11:20:41 -0500

    included p=np.inf option

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # to open toolbar, press cmd+','
 # tixz diagram
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import RegularPolygon
 import math
 import gudhi
 import scipy
@@ -38,136 +40,57 @@ class SampleData:  # TODO: create more sample shapes
         data['y coord'] = np.sin(data['radian']) + data['y noise']
 
 
-class CechComplexes:
-    def __init__(self, data_points: pd.DataFrame, time: float):
-        self.data_pts = data_points[['x coord', 'y coord']].values
-        self.time = time
-        self.complex = self.generate_cech_complex()
-
-    def generate_cech_complex(self):
-        cech_complex = []
-        DIAMETER = 2 * self.time
-        distance_matrix = scipy.spatial.distance_matrix(self.data_pts, self.data_pts, p=2)  # TODO: p=np.inf
-        for (i, j) in iter.combinations(range(len(self.data_pts)), 2):  # all lower-triangular indices
-            if distance_matrix[i][j] <= DIAMETER:
-                cech_complex.append([self.data_pts[i], self.data_pts[j]])
-        return cech_complex
-
-    def graph_complex(self):
-        COLORS = ['r', 'g', 'b', 'm']
-        lc = mc.LineCollection(self.complex, linewidths=1, color='black')
-        fig, ax = plt.subplots()
-        ax.set_aspect(1)
-        for i, (x_coord, y_coord) in enumerate(self.data_pts[['x coord', 'y coord']].values):
-            plt.scatter(x_coord, y_coord, color='black', marker='o', s=10)
-            ball = plt.Circle((x_coord, y_coord), radius=self.time, color=COLORS[i % len(COLORS)], alpha=0.2)
-            ax.add_artist(ball)
-            ax.add_collection(lc)
-        plt.show()
-
-
 class RipsComplexes:
-    def __init__(self, data_points: pd.DataFrame, time: float):
+    def __init__(self, data_points: pd.DataFrame, time: float, graph_results=True, p=np.inf):
         self.data_pts = data_points[['x coord', 'y coord']].values
-        self.time = time
+        self.time, self.p = time, p
         self.complex = self.generate_rips_complex()
-
-    def generate_rips_complex(self):
-        rips_complex = gudhi.RipsComplex(points=self.data_pts, max_edge_length=2*self.time)
-        simplex_tree = rips_complex.create_simplex_tree(max_dimension=2)
-        simplex_tree.compute_persistence()
-        return simplex_tree.get_filtration()
-
-    def plot_data_points(self, ax):
-        COLORS, OPACITY = ['r', 'g', 'b', 'm'], 0.25
-        for i, (x_coord, y_coord) in enumerate(self.data_pts):
-            plt.scatter(x_coord, y_coord, color='black', marker='o', s=10)
-            ball = plt.Circle((x_coord, y_coord), radius=self.time, color=COLORS[i % len(COLORS)], alpha=OPACITY)
-            ax.add_artist(ball)
-        # new_data = SampleData(num_data_pts=20).data[['x coord', 'y coord']].values
-        # for i, (x_coord, y_coord) in enumerate(new_data):
-        #     plt.scatter(x_coord, y_coord, color='red', marker='o', s=10)
-
-    def plot_simplices(self):
-        OPACITY = 0.20
-        for (simplex_idx, _) in self.complex:
-            simplex_coords = [self.data_pts[idx] for idx in simplex_idx]
-            simplex = plt.Polygon(simplex_coords, color='black', alpha=OPACITY, linewidth=2)
-            plt.gca().add_patch(simplex)
-
-    def graph_complex(self):
-        fig, ax = plt.subplots()
-        ax.set_aspect(1)
-        ax.autoscale(enable=True)
-        self.plot_data_points(ax)
-        self.plot_simplices()
-        plt.show()
-
-class RipsComplexes:
-    def __init__(self, data_points: pd.DataFrame, time: float):
-        self.data_pts = data_points[['x coord', 'y coord']].values
-        self.time = time
-        self.complex = self.generate_rips_complex()
-
-    def generate_rips_complex(self):
-        rips_complex = gudhi.RipsComplex(points=self.data_pts, max_edge_length=2*self.time)
-        simplex_tree = rips_complex.create_simplex_tree(max_dimension=2)
-        simplex_tree.compute_persistence()
-        return simplex_tree.get_filtration()
-
-    def plot_data_points(self, ax):
-        COLORS, OPACITY = ['r', 'g', 'b', 'm'], 0.25
-        for i, (x_coord, y_coord) in enumerate(self.data_pts):
-            plt.scatter(x_coord, y_coord, color='black', marker='o', s=10)
-            ball = plt.Circle((x_coord, y_coord), radius=self.time, color=COLORS[i % len(COLORS)], alpha=OPACITY)
-            ax.add_artist(ball)
-        # new_data = SampleData(num_data_pts=20).data[['x coord', 'y coord']].values
-        # for i, (x_coord, y_coord) in enumerate(new_data):
-        #     plt.scatter(x_coord, y_coord, color='red', marker='o', s=10)
-
-    def plot_simplices(self):
-        OPACITY = 0.20
-        for (simplex_idx, _) in self.complex:
-            simplex_coords = [self.data_pts[idx] for idx in simplex_idx]
-            simplex = plt.Polygon(simplex_coords, color='black', alpha=OPACITY, linewidth=2)
-            plt.gca().add_patch(simplex)
-
-    def graph_complex(self):
-        fig, ax = plt.subplots()
-        ax.set_aspect(1)
-        ax.autoscale(enable=True)
-        self.plot_data_points(ax)
-        self.plot_simplices()
-        plt.show()
-
-
-class Complexes:
-    def __init__(self, data_points: pd.DataFrame, time: float, complex_type='Rips', graph_results=False):
-        self.complex_type = self.get_complex_type(data_points, time, complex_type)
-        self.complex = self.complex_type.complex
         if graph_results:
             self.graph_complex()
+            plt.show()
 
-    @staticmethod
-    def get_complex_type(data_points, time, complex_type):
-        complex_type_dict = {
-            'Rips': RipsComplexes,
-            'Cech': CechComplexes
-        }
-        try:
-            return complex_type_dict[complex_type](data_points, time)
-        except KeyError:
-            print('KeyError: Shape keyword input not found.')
+    def generate_rips_complex(self):
+        distance_matrix = scipy.spatial.distance_matrix(self.data_pts, self.data_pts, p=self.p)
+        rips_complex = gudhi.RipsComplex(distance_matrix=distance_matrix, max_edge_length=2*self.time)
+        simplex_tree = rips_complex.create_simplex_tree(max_dimension=2)
+        simplex_tree.compute_persistence()
+        return simplex_tree.get_filtration()
+
+    def point_cloud_type(self, i, x_coord, y_coord):
+        COLORS, OPACITY = ['r', 'g', 'b', 'm'], 0.25
+        if self.p == np.inf:
+            return RegularPolygon((x_coord, y_coord), numVertices=4, orientation=math.pi/4, radius=self.time,
+                                  color=COLORS[i % len(COLORS)], alpha=OPACITY)
+        return plt.Circle((x_coord, y_coord), radius=self.time, color=COLORS[i % len(COLORS)], alpha=OPACITY)
+
+    def plot_data_points(self, ax):
+        for i, (x_coord, y_coord) in enumerate(self.data_pts):
+            plt.scatter(x_coord, y_coord, color='black', marker='o', s=10)
+            ball = self.point_cloud_type(i, x_coord, y_coord)
+            ax.add_artist(ball)
+        # new_data = SampleData(num_data_pts=20).data[['x coord', 'y coord']].values
+        # for i, (x_coord, y_coord) in enumerate(new_data):
+        #     plt.scatter(x_coord, y_coord, color='red', marker='o', s=10)
+
+    def plot_simplices(self):
+        OPACITY = 0.20
+        for (simplex_idx, _) in self.complex:
+            simplex_coords = [self.data_pts[idx] for idx in simplex_idx]
+            simplex = plt.Polygon(simplex_coords, color='black', alpha=OPACITY, linewidth=2)
+            plt.gca().add_patch(simplex)
 
     def graph_complex(self):
-        self.complex_type.graph_complex()
+        fig, ax = plt.subplots()
+        ax.set_aspect(1)
+        ax.autoscale(enable=True)
+        self.plot_data_points(ax)
+        self.plot_simplices()
 
 
 def main():
     sample_data = SampleData().data
     TIME = 0.15
-    rips_complex = Complexes(sample_data, TIME, complex_type='Rips', graph_results=True)
-    new_data = SampleData(num_data_pts=20).data
+    rips_complex = RipsComplexes(sample_data, TIME, graph_results=True, p=np.inf)
 
 
 if __name__ == '__main__':
